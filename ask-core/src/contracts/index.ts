1931b79096bb52d75e6ca77f2bb65e70992679af
Qarau <eurekafj@gmail.com>
2026-05-10 13:48:27 +1200

    chore(slice): close ask-projcursor-001
    ASK-Slice: ask-projcursor-001

--- ask-core/src/contracts/index.ts
+++ ask-core/src/contracts/index.ts
@@ -4,6 +4,7 @@ export * from './currentArtifactFixtures.js';
 export * from './events.js';
 export * from './eventFixtures.js';
 export * from './eventLedger.js';
+export * from './projection.js';
 export * from './tasks.js';
 export * from './taskPlanFixtures.js';
 export * from './checks.js';
